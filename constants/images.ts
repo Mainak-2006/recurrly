7c777a4add2acc91860c4ed6a1e81575e722f9f4
Mainak-2006 <mainakkundu13@gmail.com>
2026-05-23 14:47:16 +0530

    fix

--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,11 @@
 import splashPattern from '@/assets/images/splash-pattern.png'
 import avatar from '@/assets/images/avatar.png'
+import { ImageSourcePropType } from 'react-native';
+
+const images: { [key: string]: ImageSourcePropType } = {
+  splashPattern,
+  avatar,
+};
+
 
 export default { splashPattern, avatar }
